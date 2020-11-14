8b6f026a9b351b75f050e48f7e5cc64cb39a9137
Samia1234 <chasamya123@gmail.com>
2020-11-14 11:59:12 -0800

    Updates

---
+++ precache-manifest.7893ebe4e3ba949be690cd9856ecc11d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e51d391339949d71a4f4f98391c499",
+    "url": "/react_template/index.html"
+  },
+  {
+    "revision": "7205518c7cd87a671b2a",
+    "url": "/react_template/static/css/2.a3cd2708.chunk.css"
+  },
+  {
+    "revision": "c6bc9cc141bba4592dcf",
+    "url": "/react_template/static/css/main.2b2d55ed.chunk.css"
+  },
+  {
+    "revision": "7205518c7cd87a671b2a",
+    "url": "/react_template/static/js/2.e9a051fb.chunk.js"
+  },
+  {
+    "revision": "cdf166f35bbfb9c7f1a6d225ef80eb0b",
+    "url": "/react_template/static/js/2.e9a051fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6bc9cc141bba4592dcf",
+    "url": "/react_template/static/js/main.8d10478d.chunk.js"
+  },
+  {
+    "revision": "c54219b0a257102033e2",
+    "url": "/react_template/static/js/runtime-main.6ed68a65.js"
+  },
+  {
+    "revision": "64572c11be4262472f5fe051886ffbc2",
+    "url": "/react_template/static/media/assistant.64572c11.jpg"
+  },
+  {
+    "revision": "c345f5320854119120393e01b8a7c335",
+    "url": "/react_template/static/media/city.c345f532.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react_template/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react_template/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react_template/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react_template/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react_template/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "da31c713ad5de6218c1b9560c537b9a4",
+    "url": "/react_template/static/media/img1.da31c713.jpg"
+  }
+]);
